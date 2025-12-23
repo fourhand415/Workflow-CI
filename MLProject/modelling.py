ed4c8243d90b894338d178e7a17d8d940adebcc9
Muhammad Farhan Lucky Putra <63000572+fourhand415@users.noreply.github.com>
2025-12-23 13:31:14 +0700

    Update modelling.py
    Refactor model training and logging with MLflow.

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,35 +1,28 @@
-# ===== IMPORT =====
 import mlflow
+import mlflow.sklearn
 import pandas as pd
-
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-# ===== EXPERIMENT =====
-mlflow.set_experiment("CI-Retraining")
-
-# ===== LOAD DATA =====
+# Load data
 df = pd.read_csv("heart_disease_preprocessing.csv")
-
 X = df.drop("Heart Disease", axis=1)
 y = df["Heart Disease"]
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42, stratify=y
-)
-
-# ===== TRAIN + LOG =====
 with mlflow.start_run():
-    model = RandomForestClassifier(random_state=42)
-    model.fit(X_train, y_train)
+    model = RandomForestClassifier(
+        n_estimators=100,
+        max_depth=5,
+        random_state=42
+    )
 
-    y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
+    model.fit(X, y)
+    preds = model.predict(X)
 
-    mlflow.log_param("model", "RandomForestClassifier")
+    acc = accuracy_score(y, preds)
+
+    mlflow.log_param("n_estimators", 100)
+    mlflow.log_param("max_depth", 5)
     mlflow.log_metric("accuracy", acc)
-    mlflow.sklearn.log_model(model, "model")
 
-
-    print("Accuracy:", acc)
+    mlflow.sklearn.log_model(model, artifact_path="model")
