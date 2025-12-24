a3e33c837af8932c1c5aee2027dc95f938e821c1
Muhammad Farhan Lucky Putra <63000572+fourhand415@users.noreply.github.com>
2025-12-24 07:10:37 +0700

    Update modelling.py
    Removed mlflow.start_run() context manager and print statement for accuracy.

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -4,12 +4,11 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
 
-# ===== MLFLOW CONFIG =====
+# ===== EXPERIMENT =====
 mlflow.set_experiment("CI-Retraining")
 
-# ===== AUTOLOGGING =====
+# ===== AUTOLOGGING (WAJIB, SESUAI RUBRIK) =====
 mlflow.sklearn.autolog()
 
 # ===== LOAD DATA =====
@@ -23,11 +22,5 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # ===== TRAIN =====
-with mlflow.start_run():
-    model = RandomForestClassifier(random_state=42)
-    model.fit(X_train, y_train)
-
-    y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-
-    print("Accuracy:", acc)
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
